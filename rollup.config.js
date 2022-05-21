7bbf4e59b23fbf3d0b7ba5a08b08acbce180af68
LuciOroles <stoica.florin.lucian@gmail.com>
2022-05-21 19:46:53 +0300

    configure scss loading

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,7 @@ import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
 
-import postcss from "rollup-plugin-postcss";
+import scss from 'rollup-plugin-scss';
 
 const packageJson = require("./package.json");
 
@@ -26,13 +26,13 @@ export default [
       resolve(),
       commonjs(),
       typescript({ tsconfig: "./tsconfig.json" }),
-      postcss(),
+      scss(),
     ],
   },
   {
     input: "dist/esm/types/index.d.ts",
     output: [{ file: "dist/index.d.ts", format: "esm" }],
     plugins: [dts()],
-    external: [/\.css$/],
+    external: [/\.scss$/], // !important to change this
   },
 ];
